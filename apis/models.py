2efbc437c818ef4309b63bcba007e11b8bd8ad14
Marcel <kibormarcel90@gmail.com>
2021-06-06 11:36:36 +0300

    harshed user passwords

--- apis/models.py
+++ apis/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=20)
-    email = models.CharField(max_length=30, unique=True)
-    password = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
     username = None # this is done to overide the default django settings which require username for login
     
     USERNAME_FIELD = 'email' # overiding username for the email field.
